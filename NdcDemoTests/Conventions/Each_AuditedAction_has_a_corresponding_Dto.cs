2605166024c435b020962cfd76da77420e8ee9c0
Krzysztof Kozmic <krzysztof@kozmic.pl>
2012-05-06 13:04:25 +1000

    - barebone test addeed and passing

--- NdcDemoTests/Conventions/Each_AuditedAction_has_a_corresponding_Dto.cs
+++ NdcDemoTests/Conventions/Each_AuditedAction_has_a_corresponding_Dto.cs
@@ -1,7 +1,32 @@
-﻿namespace ConventionTests
+﻿using System;
+using ConsoleApplication1.AuditedActionDtos;
+using ConsoleApplication1.AuditedActions;
+
+namespace ConventionTests
 {
-    public class Each_AuditedAction_has_a_corresponding_Dto
+    public class Each_AuditedAction_has_a_corresponding_Dto:ConventionTest
     {
-         
+        private string dtoNamespace = typeof (AuditedActionDto).Namespace;
+
+        protected override ConventionData SetUp()
+        {
+            return new ConventionData
+                       {
+                           Types = IsAuditedAction,
+                           Must = HaveCorrespondingDtoType
+                       };
+            ;
+        }
+
+        private bool HaveCorrespondingDtoType(Type obj)
+        {
+            var dto = Type.GetType(dtoNamespace + "." + obj.Name + "Dto");
+            return dto != null;
+        }
+
+        private bool IsAuditedAction(Type type)
+        {
+            return type.IsAbstract == false && typeof (AuditedAction).IsAssignableFrom(type);
+        }
     }
 }
