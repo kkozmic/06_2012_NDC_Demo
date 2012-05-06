b45d5a069fbd0ec602737e31112ea109c75e6db7
Krzysztof Kozmic <krzysztof@kozmic.pl>
2012-05-06 17:08:54 +1000

    - updated a DTO to have a static member and created convention test to verify that

--- ConsoleApplication1/AuditedActionDtos/SomeAuditedActionDto.cs
+++ ConsoleApplication1/AuditedActionDtos/SomeAuditedActionDto.cs
@@ -2,5 +2,11 @@ namespace NdcDemo.AuditedActionDtos
 {
     public class SomeAuditedActionDto : AuditedActionDto
     {
+        public string Status { get; set; }
+
+        public static bool IsActive(SomeAuditedActionDto item)
+        {
+            return item != null && item.Status == "Active";
+        }
     }
 }
