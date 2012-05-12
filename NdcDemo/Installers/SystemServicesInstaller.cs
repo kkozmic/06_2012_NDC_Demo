bb9072b543b291c88212843ff5a24f5c3a4a24c6
krzysztof.kozmic <krzysztof.kozmic@gmail.com>
2012-05-12 16:38:17 +1000

    - made the test pass

---
+++ NdcDemo/Installers/SystemServicesInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace NdcDemo.Installers
+{
+    public class SystemServicesInstaller:IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<ITime>().ImplementedBy<Time>());
+        }
+    }
+}
