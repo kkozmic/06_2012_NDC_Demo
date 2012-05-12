856939f9bf34c4c61e5e1fea208fd684ebacd1aa
krzysztof.kozmic <krzysztof.kozmic@gmail.com>
2012-05-12 15:46:53 +1000

    - added failing test to check for static members on DTOs

--- NdcDemo.Tests/Conventions/Each_AuditedAction_has_a_corresponding_DTO.cs
+++ NdcDemo.Tests/Conventions/Each_AuditedAction_has_a_corresponding_DTO.cs
@@ -5,6 +5,32 @@ using NdcDemo.AuditedActions;
 
 namespace ConventionTests
 {
+    public class Each_dto_has_only_instance_members:ConventionTest
+    {
+        protected override ConventionData SetUp()
+        {
+            return new ConventionData
+                       {
+                           Types = t => t.Name.EndsWith("Dto"),
+                           Must = HaveOnlyInstanceMembers
+                       };
+        }
+
+        private bool HaveOnlyInstanceMembers(Type dto)
+        {
+            var invalidMembers = GetInvalidMembers(dto);
+            return invalidMembers.Length == 0;
+        }
+
+        private MemberInfo[] GetInvalidMembers(Type dto)
+        {
+            return dto.GetMembers(BindingFlags.Static |
+                                  BindingFlags.Public |
+                                  BindingFlags.NonPublic);
+
+        }
+    }
+
     public class Each_AuditedAction_has_a_corresponding_DTO : ConventionTest
     {
         private readonly string dtoNamespace = typeof (AuditedActionDto).Namespace;
