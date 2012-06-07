960710c18993d738414dece681f18c26c9adf74d
krzysztof.kozmic <krzysztof.kozmic@gmail.com>
2012-06-07 15:24:47 +0200

    test for orphaned dtos

---
+++ NdcDemo.Tests/Conventions/No_orphaned_audited_action_dtos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NdcDemo.AuditedActionDtos;
+using NdcDemo.AuditedActions;
+
+namespace ConventionTests
+{
+    public class No_orphaned_audited_action_dtos:ConventionTest
+    {
+        private string auditNamespace = typeof (AuditedAction).Namespace;
+        protected override ConventionData SetUp()
+        {
+            return new ConventionData
+                {
+                    Types = t => t.IsConcrete<AuditedActionDto>(),
+                    Must = HaveCorrespondingAudit,
+                    FailDescription = "There are orphaned audit dtos"
+                };
+        }
+
+        private bool HaveCorrespondingAudit(Type obj)
+        {
+            var aaName = GetAuditName(obj);
+            return GetAuditType(aaName) != null;
+        }
+
+        private Type GetAuditType(string aaName)
+        {
+            return typeof (AuditedAction).Assembly.GetType(aaName);
+        }
+
+        private string GetAuditName(Type type)
+        {
+            return auditNamespace + "." + type.Name.Replace("Dto", string.Empty);
+        }
+    }
+}
