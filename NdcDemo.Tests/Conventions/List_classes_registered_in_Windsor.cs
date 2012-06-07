fbae5621ac3d44eb18ea3cb141b10e7234876094
krzysztof.kozmic <krzysztof.kozmic@gmail.com>
2012-06-07 19:51:50 +0200

    listing components in Windsor

---
+++ NdcDemo.Tests/Conventions/List_classes_registered_in_Windsor.cs
@@ -0,0 +1,22 @@
+﻿using Castle.Windsor;
+using Castle.Windsor.Diagnostics.Helpers;
+using Castle.Windsor.Installer;
+using NdcDemo.AuditedActions;
+
+namespace ConventionTests
+{
+    public class List_classes_registered_in_Windsor:WindsorConventionTest
+    {
+        protected override WindsorConventionData SetUp()
+        {
+            return new WindsorConventionData(new WindsorContainer()
+                                                 .Install(FromAssembly.Containing<AuditedAction>()))
+                {
+                    FailDescription = "All Windsor components",
+                    FailItemDescription = h => BuildDetailedHandlerDescription(h)+" | "+h.ComponentModel.GetLifestyleDescription(),
+                    HasApprovedExceptions = true
+                };
+
+        }
+    }
+}
