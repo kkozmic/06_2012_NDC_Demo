78812bf18f5a0878c9641ee9884a2cbac3a7c320
krzysztof.kozmic <krzysztof.kozmic@gmail.com>
2012-06-07 15:10:49 +0200

    first barebone test

---
+++ NdcDemo.Tests/Conventions/Each_AuditedAction_has_a_dto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NdcDemo.AuditedActionDtos;
+using NdcDemo.AuditedActions;
+
+namespace ConventionTests
+{
+    public class Each_AuditedAction_has_a_dto : ConventionTest
+    {
+        private readonly string dtoNamespace = typeof (AuditedActionDto).Namespace;
+
+        protected override ConventionData SetUp()
+        {
+            return new ConventionData
+                {
+                    Types = t => t.IsConcrete<AuditedAction>(),
+                    Must = HaveCorrespondingDto,
+                };
+        }
+
+        private bool HaveCorrespondingDto(Type auditedAction)
+        {
+            string dtoTypeName = GetDtoTypeName(auditedAction);
+            Type dtoType = GetDtoType(dtoTypeName);
+            return dtoType != null;
+        }
+
+        private string GetDtoTypeName(Type auditedAction)
+        {
+            return dtoNamespace + "." + auditedAction.Name + "Dto";
+        }
+
+        private Type GetDtoType(string dtoTypeName)
+        {
+            return typeof (AuditedActionDto).Assembly.GetType(dtoTypeName);
+        }
+    }
+}
